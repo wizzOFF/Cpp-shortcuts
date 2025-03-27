6016eb77238574231aa5b432e7017e293dde67d9
WizzOFF <maher_off@proton.me>
2025-03-27 03:49:47 +0100

    shortcuts.cpp: mathematical functions > power function

--- shortcuts.cpp
+++ shortcuts.cpp
@@ -12,7 +12,7 @@ int main()
    return 0;
 }
 */
-
+/*
 #include <iostream>
 #include <cmath>  //Ne pas oublier cette ligne 
 using namespace std;
@@ -28,4 +28,33 @@ int main()
     cout << "La racine de " << nombre << " est " << resultat << endl;
 
     return 0;
+}
+*/
+
+#include <iostream>
+#include <cmath>  //Ne pas oublier !
+using namespace std;
+
+int main()
+{
+   double a(0), b(0); //Déclaration des variables utiles
+
+   cout << "Bienvenue dans le programme de calcul de a^b !" << endl;
+
+   cout << "Donnez une valeur pour a : "; //On demande le premier nombre
+   cin >> a;
+
+   cout << "Donnez une valeur pour b : "; //On demande le deuxième nombre
+   cin >> b;
+
+   double const resultat(pow(a, b)); //On effectue l'opération
+   //On peut aussi écrire comme avant :
+   //double const resultat = pow(a,b);
+   //Souvenez-vous des deux formes possibles
+   //De l'initialisation d'une variable
+
+   cout << a << " ^ " << b << " = " << resultat << endl;
+   //On affiche le résultat
+
+   return 0;
 }
