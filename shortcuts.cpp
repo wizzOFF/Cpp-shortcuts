dcbed2e4feea95b82e3e632eae87a0b7ae1fd24c
WizzOFF <maher_off@proton.me>
2025-03-27 03:36:22 +0100

    shortcuts.cpp: remove 1 from a variable with decrement

--- shortcuts.cpp
+++ shortcuts.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main()
 {
    int nombreJoueur(4); //Il y a 4 joueurs dans la partie
-   // nombreJoueur = nombreJoueur + 1;
-   ++nombreJoueur;
+   // nombreJoueur = nombreJoueur - 1;
+   --nombreJoueur;
    //À partir d'ici, il y a 5 joueurs
    return 0;
 }
