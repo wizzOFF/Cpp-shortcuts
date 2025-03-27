6789d850a2b2831c2f393e0d67b00eaf4067f1ee
WizzOFF <maher_off@proton.me>
2025-03-27 03:46:13 +0100

    shortcuts.cpp: mathematical functions > square root function

--- shortcuts.cpp
+++ shortcuts.cpp
@@ -1,4 +1,4 @@
-
+/*
 #include <iostream>
 using namespace std;
 
@@ -10,4 +10,22 @@ int main()
    nombre -= 1.;        //'nombre' vaut maintenant 18
    nombre /= 3.;        //'nombre' vaut maintenant 6
    return 0;
+}
+*/
+
+#include <iostream>
+#include <cmath>  //Ne pas oublier cette ligne 
+using namespace std;
+
+int main()
+{
+    double const nombre(16); //Le nombre dont on veut la racine
+                             //Comme sa valeur ne changera pas on met 'const'
+    double resultat;         //Une case mémoire pour stocker le résultat
+
+    resultat = sqrt(nombre);  //On effectue le calcul !
+
+    cout << "La racine de " << nombre << " est " << resultat << endl;
+
+    return 0;
 }
