45dd92444b57468dfb50f5bffe116a574eca1cff
WizzOFF <maher_off@proton.me>
2025-03-27 03:32:49 +0100

    shortcuts.cpp: add 1 to a variable with increment

---
+++ shortcuts.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int nombreJoueur(4); //Il y a 4 joueurs dans la partie
+   // nombreJoueur = nombreJoueur + 1;
+   ++nombreJoueur;
+   //À partir d'ici, il y a 5 joueurs
+   return 0;
+}
